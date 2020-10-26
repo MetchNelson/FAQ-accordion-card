dcf77eda40ac6d612e98da9016412ad80502da96
Eli Falak <metchnelson@gmail.com>
2020-10-26 14:07:37 -0400

    debugging

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-function slider() {
-    console.log(5);
+// document.getElementById('answer1').style.display = 'none'
+function show() {
+    answer = document.getElementsById('answer1')
+    if (answer.style.display === 'none') {
+        answer.style.display === 'block';
+    } else {
+        answer.style.display === 'none';
+    }
 }
