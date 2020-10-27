aa8392537415551384c777bb94ba13728e1466ec
Eli Falak <metchnelson@gmail.com>
2020-10-26 21:19:58 -0400

    fixing

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 function show() {
     const answer = document.getElementById('answer1');
-    answer.style.display = 'none';
     if (answer.style.display === 'block') {
         answer.style.display = 'none';
     } else {
