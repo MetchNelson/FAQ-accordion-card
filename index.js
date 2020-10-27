b8ba77698b091eab73ddc34ea625f193af9a4282
Eli Falak <metchnelson@gmail.com>
2020-10-26 21:08:38 -0400

    degugging code

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function show() {
     answer = document.getElementById('answer1')
     answer.style.display = 'none';
     if (answer.style.display === 'block') {
-        return answer.style.display === 'none';
+        answer.style.display = 'none';
     } else {
-        answer.style.display === 'block';
+        answer.style.display = 'block';
     }
 }
