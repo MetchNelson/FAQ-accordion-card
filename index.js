e4f875c939e676db9c58ada6c3ddb47798bf88b3
Eli Falak <metchnelson@gmail.com>
2020-10-26 21:17:59 -0400

    fixing code

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function show() {
-    answer = document.getElementById('answer1')
+    const answer = document.getElementById('answer1');
     answer.style.display = 'none';
     if (answer.style.display === 'block') {
         answer.style.display = 'none';
