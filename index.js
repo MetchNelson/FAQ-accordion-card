68d282a33fd87939b5b5b437758e94965defa9b7
Eli Falak <metchnelson@gmail.com>
2020-10-26 21:30:14 -0400

    adding funcitnality

--- index.js
+++ index.js
@@ -6,3 +6,39 @@ function show() {
         answer.style.display = 'block';
     }
 }
+
+function show2() {
+    const answer = document.getElementById('secondQ');
+    if (answer.style.display === 'block') {
+        answer.style.display = 'none';
+    } else {
+        answer.style.display = 'block';
+    }
+}
+
+function show3() {
+    const answer = document.getElementById('thirdQ');
+    if (answer.style.display === 'block') {
+        answer.style.display = 'none';
+    } else {
+        answer.style.display = 'block';
+    }
+}
+
+function show4() {
+    const answer = document.getElementById('fourthQ');
+    if (answer.style.display === 'block') {
+        answer.style.display = 'none';
+    } else {
+        answer.style.display = 'block';
+    }
+}
+
+function show5() {
+    const answer = document.getElementById('fifthQ');
+    if (answer.style.display === 'block') {
+        answer.style.display = 'none';
+    } else {
+        answer.style.display = 'block';
+    }
+}
