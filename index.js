f75280d0467302aaf6e694b0bdd9165dc37a521c
Eli Falak <metchnelson@gmail.com>
2020-10-27 13:41:38 -0400

    testing the css

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+/* This is the function for the buttton
+that shows and hides the answers to the question. 
+I had to make one for each button because everytime 
+I put them all in a class the code would brake. Can
+anyone help? */
+
 function show() {
     const answer = document.getElementById('answer1');
     if (answer.style.display === 'block') {
